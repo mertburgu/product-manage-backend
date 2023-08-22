c23dcfe86d2ed96173304d877cda76becc509c21
mert can burgu <mertburgu@yahoo.com>
2023-08-22 21:28:01 +0300

    fix

--- src/models/Product.js
+++ src/models/Product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     name: {type: String, required: false, min: 3, max: 255},
     description: {type: String, required: false, min: 3, max: 255},
     productCode: {type: String, required: true, min: 3, max: 255},
-    quantity: {type: Number, required: true, default:0},
+    quantity: {type: Number, default:0},
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
 });
